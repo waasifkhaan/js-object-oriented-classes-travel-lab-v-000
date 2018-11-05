8b7c2f05473519ea18203d1665a47a002e9a5e59
Wasif Khan <waasif.khaan@gmail.com>
2018-11-05 16:53:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class Driver {
   constructor(name, startdate) {
     this.name = name;
-    this.startDate = new Date;
+    this.startDate = new Date(startdate);
   }
   yearsExperienceFromBeginningOf(year) {
     return year - this.startDate.getFullYear();
