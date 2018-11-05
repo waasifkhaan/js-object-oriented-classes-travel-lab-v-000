bd8bcc0bd8d319475b832acb17378e9285ff0c06
Wasif Khan <waasif.khaan@gmail.com>
2018-11-05 17:55:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+class Driver {
+  constructor(name, startdate) {
+    this.name = name;
+    this.startDate = new Date(startdate);
+  }
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear();
+  }
+}
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
 
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+  avenueToInteger(avenue) {
+    return eastWest.indexOf(avenue);
+  }
+  blocksTravelled() {
+    let horizontalDistance =
+      this.avenueToInteger(this.endingLocation.horizontal) -
+      this.avenueToInteger(this.beginningLocation.horizontal);
+    let verticalDistance =
+      this.endingLocation.vertical - this.beginningLocation.vertical;
+    return Math.abs(horizontalDistance) + Math.abs(verticalDistance);
+  }
+
+  estimatedTime(peak) {
+    if (peak){
+      return this.blocksTravelled ()/2 ;}
+    else {
+      return this.blocksTravelled ()/3 ;
+    }
+  }
+
+}
