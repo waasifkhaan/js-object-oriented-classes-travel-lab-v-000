c6493c733c4a53beac3307f338c014ecdbb6a8b6
Wasif Khan <waasif.khaan@gmail.com>
2018-11-05 16:58:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ class Driver {
     return year - this.startDate.getFullYear();
 }
 }
+
+class Route { 
+  constructor(x,y){
+      this.beginningLocation = x;
+      this.endingLocation = y;
+  }
+  
+}
