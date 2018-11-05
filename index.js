08c0c2f41c8b1fca34eb274fb2d64a98f88d1818
Wasif Khan <waasif.khaan@gmail.com>
2018-11-05 17:33:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,17 +8,4 @@ class Driver {
   }
 }
 
-class Route {
-  constructor(x,y){
-      this.beginningLocation = x;
-      this.endingLocation = y;
-}
 
-let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
-x = eastWest.findIndex(function (element){ element === this.beginningLocation.horizontal})
-y = eastWest.findIndex(function (element) { element === this.endingLocation.horizontal})
-  blocksTravelled(){
-return (x - y ) + (this.beginningLocation.vertical -this.endingLocation.vertical);
-  }
-
-}
