9779f30217cb3d196f62945915ce36952d7bf10b
Wasif Khan <waasif.khaan@gmail.com>
2018-11-05 16:38:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+class Driver {
+  constructor(name, string ) {
+    this.name = name;
+    this.homeState = string;
+    
+  }
+
+  veto() {
+    return `No, I must disagree`;
+  }
+  approve() {
+    return `You can do that!`;
+  }
