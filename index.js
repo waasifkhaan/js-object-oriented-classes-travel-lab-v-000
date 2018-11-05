f4d2a316b8a1d6d0fe692fb4b4067e56aba6a28d
Wasif Khan <waasif.khaan@gmail.com>
2018-11-05 17:38:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,11 @@ class Route {
 }
 
 let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
-x = eastWest.findIndex(function (element){ element === this.beginningLocation.horizontal})
+t = eastWest.findIndex(function (element){ element === this.beginningLocation.horizontal})
 y = eastWest.findIndex(function (element) { element === this.endingLocation.horizontal})
-
+t = eastWest.indexOf(this.beginningLocation.horizontal)
+s = eastWest.indexOf(this.endingLocation.horizontal)
+}
   blocksTravelled(){
     return Math.abs(x - y ) + Math.abs(this.beginningLocation.vertical -this.endingLocation.vertical);
   }
