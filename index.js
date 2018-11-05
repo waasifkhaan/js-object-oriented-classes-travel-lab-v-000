6bfed0ed1cc72011ae13575751b40935f6349ba8
Wasif Khan <waasif.khaan@gmail.com>
2018-11-05 17:23:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,25 +13,9 @@ class Route {
       this.beginningLocation = x;
       this.endingLocation = y;
 }
-let array2_large= ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
-array_1_small = this.beginningLocation.horizontal
-function findMatch(array_1_small, array2_large) {
-var ary = new Array();
-for(i = 0;i < array2_large.length; i++)
-{
-  for(z = 0; z < array_1_small.length; z++)
-  {
-    if(array2_large[i] == array_1_small[z])
-    {
-      ary.push(i);
-    }
-  }
 
-}
-return ary;
-}
 blocksTravelled(){
-return (this.beginningLocation.horizontal - this.endingLocation.horizontal) + (this.beginningLocation.vertical -this.endingLocation.vertical);
+return (this.beginningLocation.horizontal - this.endingLocation.horizontal) + (this.beginningLocation.vertical -this.endingLocation.vertical); 
 }
 estimatedTime(){
 
