4e31bb50c935da63efbe12d67a641e4d8bcbe856
Wasif Khan <waasif.khaan@gmail.com>
2018-11-05 16:40:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ class Driver {
   constructor(name, startdate ) {
     this.name = name;
     this.startDate = startdate;
-    
+
   }
 
   veto() {
     return `No, I must disagree`;
   }
-  approve() {
-    return `You can do that!`;
-  }
+}
